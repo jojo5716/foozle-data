5890562cab41c08a35e34463b77882a732607931
Jonathan Rodríguez Alejos <jonathan.rodriguez@roiback.com>
2018-09-14 23:26:59 +0200

    Creating initial django project

---
+++ data/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.http import JsonResponse
+from django.shortcuts import render
+
+def track_data(request):
+    return JsonResponse({
+        success: True
+    })
