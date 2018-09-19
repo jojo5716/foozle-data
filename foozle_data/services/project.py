d018f99b2343d171962ad40bf1c43937bc1c1dbd
Jonathan Rodríguez Alejos <jonathan.rodriguez@roiback.com>
2018-09-19 14:49:12 +0200

    adding ValidationError as exception

--- foozle_data/services/project.py
+++ foozle_data/services/project.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+
 from ..apps.data.models import Project
 
 def get_project_by_uuid(project_uuid):
@@ -12,7 +14,7 @@ def get_project_by_uuid(project_uuid):
     """
     try:
         project = Project.objects.get(unique_id=project_uuid)
-    except Project.DoesNotExist:
+    except (ValidationError, Project.DoesNotExist):
         project = None
     
     return project
