66e42d71f296fae2dcb37694af2e27284d128749
Jonathan Rodríguez Alejos <jonathan.rodriguez@roiback.com>
2018-09-19 17:38:46 +0200

    adding exception to convert datetime

--- foozle_data/helpers/date.py
+++ foozle_data/helpers/date.py
@@ -4,5 +4,5 @@ from datetime import datetime
 def convert_iso_string_date(iso_date):
     try:
         return datetime.strptime(iso_date, '%Y-%m-%dT%H:%M:%S.%fZ')
-    except ValueError:
+    except (ValueError, TypeError):
         return datetime.now()
