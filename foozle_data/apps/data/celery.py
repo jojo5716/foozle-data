651e3228c4109c701b9b486208312545bee9613f
Jonathan Rodríguez Alejos <jonathan.rodriguez@roiback.com>
2018-09-19 16:27:28 +0200

    adding celery delay

---
+++ foozle_data/apps/data/celery.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import json
+import datetime
+
+from django.db import transaction
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from celery.decorators import task
+
+from ...services import (
+    user as user_service,
+    navigation as navigation_service,
+    page as page_service,
+    bookings as bookings_service,
+    availabilities as availability_service,
+    action as action_service
+)
+
+from pprint import pprint
+
+@task(name="saving_data")
+@transaction.atomic
+def register_data(project, body_json):
+    def register_array_field(instance, field_name, value):
+        if value:
+            values = getattr(instance, field_name, [])
+            values.append(value)
+
+
+    if project:
+        # Getting user visitor profile
+        pprint(body_json)
+        user_session = body_json["data"]["session"]
+        user, user_created = user_service.get_or_create(user_session)
+
+        # Generating a new page, if this page it's same that the before,
+        # is beacause user reload the page.
+        page = page_service.create_page(body_json["data"])
+    
+        booking = bookings_service.create_or_update(body_json["data"]["booking"])
+
+        availability = availability_service.create(body_json["data"])
+        
+        # Try to get the navigation or create one if does not exist (first request for the user)
+        navigation_query = {
+            "user": user,
+            "project": project,
+            "session_id": body_json["data"]["sessionTemp"],
+            "user_info": body_json["data"]["userInfo"],
+        }
+
+        navigation, navCreated = navigation_service.get_or_create(navigation_query, page)
+
+        navigation.pages.add(page)
+        if booking:
+            navigation.bookings.add(booking)
+        
+        if availability:
+            navigation.availabilities.add(availability)
+
+        navigation.last_view = datetime.datetime.now()
+
+        navigation.save()
--- foozle_data/apps/data/views.py
+++ foozle_data/apps/data/views.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import json
-import datetime
 
 from django.http import JsonResponse
-from django.shortcuts import render
 
 from ...services import (
     project as project_service,
-    user as user_service,
-    navigation as navigation_service,
     page as page_service,
-    bookings as bookings_service,
-    availabilities as availability_service,
     action as action_service
 )
+
+from .celery import register_data
 
 from pprint import pprint
 
@@ -48,11 +44,6 @@ def track_data(request):
         u'project': u'09da745a-6df1-9326-445a-bbc659b97642e'}
 
     """
-    def register_array_field(instance, field_name, value):
-        if value:
-            values = getattr(instance, field_name, [])
-            values.append(value)
-
     if request.method == "POST":
         body_json = json.loads(request.body)
         project = project_service.get_project_by_uuid(body_json["project"])
@@ -60,41 +51,8 @@ def track_data(request):
         if project:
             # Getting user visitor profile
             pprint(body_json)
-            user_session = body_json["data"]["session"]
-            user, user_created = user_service.get_or_create(user_session)
-
-            # Generating a new page, if this page it's same that the before,
-            # is beacause user reload the page.
-            page = page_service.create_page(body_json["data"])
-     
-            booking = bookings_service.create_or_update(body_json["data"]["booking"])
-
-            availability = availability_service.create(body_json["data"])
-            
-            # Try to get the navigation or create one if does not exist (first request for the user)
-            navigation_query = {
-                "user": user,
-                "project": project,
-                "session_id": body_json["data"]["sessionTemp"],
-                "user_info": body_json["data"]["userInfo"],
-            }
-
-            navigation, navCreated = navigation_service.get_or_create(navigation_query, page)
-
-            navigation.pages.add(page)
-            if booking:
-                navigation.bookings.add(booking)
-            
-            if availability:
-                navigation.availabilities.add(availability)
-
-            navigation.last_view = datetime.datetime.now()
-
-            navigation.save()
-
-        return JsonResponse({
-            "success": True
-        })
+            register_data.delay(body_json)
+       
 
 
 def track_action(request):
